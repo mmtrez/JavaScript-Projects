1f4e748fc3aea70d35634aacf9ac62d4043bfb13
mmtrez <immtrez@gmail.com>
2022-08-13 17:44:26 +0430

    game logic

--- index.js
+++ index.js
@@ -12,3 +12,37 @@ const getPlayerChoice = () => {
   ).toLowerCase();
   return playerChoice;
 };
+
+const playRound = (playerSelection, computerSelection) => {
+  switch (playerSelection + "-" + computerSelection) {
+    case "rock-rock":
+      console.log("Draw! Rock against Rock");
+      break;
+    case "rock-paper":
+      console.log("You Lose! Paper beats Rock.");
+      break;
+    case "rock-scissors":
+      console.log("You Won! Rock beats Scissors.");
+      break;
+    case "paper-rock":
+      console.log("You Won! Paper beats Rock.");
+      break;
+    case "paper-paper":
+      console.log("Draw! Paper against paper");
+      break;
+    case "paper-scissors":
+      console.log("You Lose! Scissors beats Paper.");
+      break;
+    case "scissors-rock":
+      console.log("You Lose! Rock beats Scissors.");
+      break;
+    case "scissors-paper":
+      console.log("You Won! Scissors beats Paper.");
+      break;
+    case "scissors-scissors":
+      console.log("Draw! Scissors against Scissors.");
+      break;
+  }
+};
+
+playRound(getPlayerChoice(), getComputerChoice());
