181c703b013474e43a3f1593c0767094dd0ec59a
mmtrez <immtrez@gmail.com>
2022-08-15 06:42:42 +0430

    clean code - fix removing class

--- Drum-Kit/index.js
+++ Drum-Kit/index.js
@@ -1,18 +1,31 @@
 "use strict";
 const keys = document.querySelectorAll(".key");
-keys.forEach((key) => {
-  key.addEventListener("transitionend", (e) => {
-    if (e.propertyName === "transform") {
-      key.classList.remove("playing");
-    }
-  });
-});
-window.addEventListener("keydown", (e) => {
-  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+
+function playOnClick() {
+  const key = this.getAttribute("data-key");
+  const audio = document.querySelector(`audio[data-key="${key}"]`);
   if (audio) {
-    const key = document.querySelector(`.key[data-key="${e.key}"]`);
-    key.classList.add("playing");
+    this.classList.toggle("playing");
     audio.currentTime = 0; // to replay the audio
     audio.play();
   }
+}
+const playOnKey = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  if (audio) {
+    const key = document.querySelector(`.key[data-key="${e.key}"]`);
+    key.classList.toggle("playing");
+    audio.currentTime = 0; // to replay the audio
+    audio.play();
+  }
+};
+function removeClass(e) {
+  if (e.propertyName === "transform") {
+    this.classList.remove("playing");
+  }
+}
+window.addEventListener("keydown", playOnKey);
+keys.forEach((key) => {
+  key.addEventListener("click", playOnClick);
+  key.addEventListener("transitionend", removeClass);
 });
